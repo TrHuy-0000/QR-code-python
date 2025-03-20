2527394a731912600c42abed2ecbe14e835bf314
TrHuy-0000 <huy36567@gmail.com>
2025-03-20 12:50:11 +0700

    cập nhật code

--- codeqr/QR.py
+++ codeqr/QR.py
@@ -1,7 +1,8 @@
 import qrcode
 
 def generate_common_qr():
-    url = "http://127.0.0.1:5000"
+    url = "https://qr-code-python.onrender.com"
+
     qr = qrcode.make(url)
     qr.save("common_qr.png")
     print("Mã QR chung đã được tạo! Quét để mở trang điểm danh.")
